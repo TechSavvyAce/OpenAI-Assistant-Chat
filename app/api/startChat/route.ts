1c3c02190fb7c1889f5eb92f86d2ebf6962130c0
unknown <john0817tilley@gmail.com>
2023-12-03 13:12:44 -0500

    updated

--- app/api/startChat/route.ts
+++ app/api/startChat/route.ts
@@ -84,7 +84,7 @@ export async function POST(req: NextRequest) {
       // Poll for the completion of the run
       let completedRun;
       do {
-        await new Promise((resolve) => setTimeout(resolve, 10000)); // Delay for 1 second
+        await new Promise((resolve) => setTimeout(resolve, 15000)); // Delay for 1 second
 
         completedRun = await openai.beta.threads.runs.retrieve(
           threadId,
